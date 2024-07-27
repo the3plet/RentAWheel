3f788ef38d50ee1b444eeff48fa74b828639e97b
the3plet <soorajp2002@gmail.com>
2024-07-27 11:14:57 +0530

    home styled + layout fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       fontFamily: {
         // Add your Google Font here
-        title: ['Barlow Condensed'],
-        footer:["Zain"]
+        title: ["Barlow Condensed"],
+        footer: ["Zain"],
+        carlist:["DM Sans"],
       },
       keyframes: {
         roll: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
       animation: {
-        roll: 'roll 15s linear infinite',
+        roll: "roll 15s linear infinite",
+        rollLg: "roll 25s linear infinite",
       },
     },
   },
   plugins: [],
-}
-
+};
