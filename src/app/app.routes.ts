146201adffedfd85a69bcf786276ea5be88a867c
the3plet <soorajp2002@gmail.com>
2024-07-29 08:29:10 +0530

    add-car added and netlify.toml redirect changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { CarDetailsComponent } from './car-details/car-details.component';
 import { FindBookingsComponent } from './find-bookings/find-bookings.component';
 import { NotfoundComponent } from './layout/notfound/notfound.component';
 import { BookingsComponent } from './bookings/bookings.component';
+import { AddCarComponent } from './add-car/add-car.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'car/:id', component: CarDetailsComponent },
-  { path: 'find-bookings',component:FindBookingsComponent },
-  {path:'rental-bookings',component:BookingsComponent},
+  { path: 'find-bookings', component: FindBookingsComponent },
+  { path: 'rental-bookings', component: BookingsComponent },
+  {path:'add-car',component:AddCarComponent},
   { path: '**', component: NotfoundComponent },
 ];
 
